17729fb484ade4b0e2386e00177bbe78c07f001e
NQVinh <qvinhkl10@gmail.com>
2025-09-25 15:10:17 +0700

    feat: add exception handling classes and register middleware

--- MovieReservation.Server/Program.cs
+++ MovieReservation.Server/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.IdentityModel.Tokens;
 using MovieReservation.Server.Application;
 using MovieReservation.Server.Infrastructure;
 using MovieReservation.Server.Infrastructure.Services;
+using MovieReservation.Server.Web.Middleware;
 using StackExchange.Redis;
 using System;
 
@@ -12,10 +13,29 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    //IgnoreCycles để tránh vòng lặp khi serialize
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.WriteIndented = true;
+        // Cấu hình để serialize enum dưới dạng string thay vì số
+        // options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// // Cấu hình CORS
+// builder.Services.AddCors(options =>
+// {
+//     options.AddPolicy("AllowAll", policy =>
+//     {
+//         policy.AllowAnyOrigin()
+//               .AllowAnyMethod()
+//               .AllowAnyHeader();
+//     });
+// });
 // Gọi extension method đã tạo trong DependencyInjection.cs
 builder.AddInfrastructureServices();
 builder.AddApplicationServices();
@@ -33,6 +53,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// // Sử dụng CORS
+// app.UseCors("AllowAll");
+
+// Thêm middleware xử lý ngoại lệ
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.UseAuthentication();
 app.UseAuthorization();
