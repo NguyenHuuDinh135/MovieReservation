ae0c42f3f63f4d15252187ecce7ba2d9220842b2
Nick_MinhTue <23115053122342@sv.ute.udn.vn>
2025-10-05 23:53:00 +0700

    add comment DependencyInjection.cs

--- MovieReservation.Server/Infrastructure/DependencyInjection.cs
+++ MovieReservation.Server/Infrastructure/DependencyInjection.cs
@@ -8,33 +8,44 @@ using StackExchange.Redis;
 
 namespace MovieReservation.Server.Infrastructure
 {
+    // Lớp mở rộng để đăng ký và cấu hình các dịch vụ tầng hạ tầng vào Dependency Injection
     public static class DependencyInjection
     {
+        // cấu hình tất cả service
         public static void AddInfrastructureServices(this IHostApplicationBuilder builder)
         {
+            // cấu hình Dbcontext để kết nối đến csdl
             builder.Services.AddDbContext<MovieReservationDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            // cấu hình redis
             builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
             {
                 var configuration = ConfigurationOptions.Parse(builder.Configuration.GetValue<string>("Redis:ConnectionString"), true);
-                configuration.AbortOnConnectFail = false;
+                // không dừng nếu redis chưa sẵn sàng
+                configuration.AbortOnConnectFail = false; 
                 return ConnectionMultiplexer.Connect(configuration);
             });
 
+            // seeder data
             builder.Services.AddScoped<MovieReservationDbContextInitialiser>();
 
+            // cấu hình ASP.Net
             builder.Services.AddIdentity<User, IdentityRole>(options =>
             {
-                options.Password.RequiredLength = 6;
-                options.Password.RequireDigit = false;
-                options.Password.RequireUppercase = false;
-                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 6; // độ dài tối thiểu
+                options.Password.RequireDigit = false; // không bắt buộc có số
+                options.Password.RequireUppercase = false; // khôg bắt buộc in hoa
+                options.Password.RequireNonAlphanumeric = false; // không bắt buộc ký tự đặt biệt
 
+                // xác thực email trước khi đăng nhập
                 options.SignIn.RequireConfirmedEmail = true;
             })
+            // Kết nối Identity với DbContext
             .AddEntityFrameworkStores<MovieReservationDbContext>()
+             // Thêm bộ tạo Token mặc định
             .AddDefaultTokenProviders();
 
+            // Cấu hình Authentication sử dụng JWT Bearer
             builder.Services.AddAuthentication(options =>
             {
                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -42,21 +53,23 @@ namespace MovieReservation.Server.Infrastructure
             })
             .AddJwtBearer(options =>
             {
-                options.SaveToken = true;
+                options.SaveToken = true; // Lưu token trong HttpContext khi xác thực thành công
                 options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
                 {
                     ClockSkew = TimeSpan.Zero, // Không cho phép trễ hạn
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
+                    ValidateIssuer = true, // Xác minh Issuer
+                    ValidateAudience = true, // Xác minh Audience
+                    ValidateLifetime = true, // Kiểm tra hạn token
                     ValidateIssuerSigningKey = true,
                     ValidIssuer = builder.Configuration["Jwt:Issuer"],
                     ValidAudience = builder.Configuration["Jwt:Audience"],
                     IssuerSigningKey = new SymmetricSecurityKey(
+                        // Khóa bí mật ký token
                         System.Text.Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Secret"]))
                 };
             });
 
+            // Bật Authorization
             builder.Services.AddAuthorization();
 
             //custom services
@@ -70,8 +83,8 @@ namespace MovieReservation.Server.Infrastructure
                 options.Cookie.HttpOnly = true;
                 options.Cookie.SecurePolicy = CookieSecurePolicy.Always; // chỉ gửi qua HTTPS
                 options.Cookie.SameSite = SameSiteMode.Strict; // chống CSRF
-                options.ExpireTimeSpan = TimeSpan.FromDays(7);
-                options.SlidingExpiration = false;
+                options.ExpireTimeSpan = TimeSpan.FromDays(7); //cookie hết hạn sau 7 ngày
+                options.SlidingExpiration = false; không gia hạn thời gian
             });
         }
 
