4e001d32c45f238e41b294fefe305ee4d8c647da
NguyenMTue <23115053122342@sv.ute.udn.vn>
2025-10-17 17:25:25 +0700

    fix

--- MovieReservation.Server/Application/Genres/Queries/GetGenreById/GenreByIdDto.cs
+++ MovieReservation.Server/Application/Genres/Queries/GetGenreById/GenreByIdDto.cs
@@ -9,15 +9,6 @@ namespace MovieReservation.Server.Application.Genres.Queries.GetGenresById
 {
     public class GetGenresById
     {
-        public int Id { get; init; }
-        public string Name { gett; init; }
 
-        public class Mapping : Profile
-        {
-            public Mapping()
-            {
-                CreateMap<Genre, GetGenresByIdDto>();
-            }
-        }
     }
 }
